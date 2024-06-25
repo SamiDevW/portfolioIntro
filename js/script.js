13275b723c6fbfdda3baceddd872cd862e427ca2
SamiDevW <SamiDevW@users.noreply.github.com>
2024-06-25 22:57:29 +0200

    responsive

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@
 import { appearingBlock } from "./appearingBlock.js";
 import { typingText } from "./typingText.js";
 document.addEventListener("DOMContentLoaded", function () {
+    const isLargeScreen = window.matchMedia("(min-width: 768px)").matches;
 
     typingText();
+    if (!isLargeScreen) {
+        return; // Exit the function if the screen size is smaller than 768px
+    }
     appearingBlock();
 });
